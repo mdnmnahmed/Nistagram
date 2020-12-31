e18d4726285a9c91438fd8ecbd8490615241087a
Numan Ahmed <mdnmnahmed@gmail.com>
2021-01-01 00:56:07 +0530

    ignore key files

--- server/keys.js
+++ server/keys.js
@@ -2,4 +2,3 @@ module.exports = {
     MONGOURI: "mongodb+srv://nistagram:nistagramMONGODB@cluster0.dfcda.mongodb.net/nistagram?retryWrites=true&w=majority",
     JWT_SECRET: "namunMdemhamargatsni"
 }
-
