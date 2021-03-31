759d16da07cf0ad8d5bc7c5a8b6f22b0f19d1042
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-31 07:24:45 +0530

    removed few old files

--- server/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://nistagram:nistagramMONGODB@cluster0.dfcda.mongodb.net/nistagram?retryWrites=true&w=majority",
-    JWT_SECRET: "namunMdemhamargatsni",
-    JWT_SECRET: "namunMdemhamargatsni"
-}
