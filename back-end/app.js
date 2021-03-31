e131657c2412498a97a30ffb9fd12130fd30d061
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-31 07:26:35 +0530

    created server & maked mongoDB connection

---
+++ back-end/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
+const app = express();
+
+
+//Middleware
+app.use(express.json()); //Top at middleware
+
+
+//Routers
+app.use('/', require('./routes/auth/auth'));
+
+//MongoDB Connection
+mongoose.connect(process.env.DB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.connection.on('connected', () => {
+    console.log("Connected With DB");
+});
+mongoose.connection.on('error', (err) => {
+    console.log("Errot Occured while Connecting with DB : " + err);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Nistagram Started');
+});
