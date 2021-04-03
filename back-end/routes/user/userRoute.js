df4cd08680ddbfbea9976f0110b30872a9b774c2
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-03 06:46:55 +0530

    signup route created

---
+++ back-end/routes/user/userRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { signup, signin } = require('../../controllers/user/userController');
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/signin', signin);
+
+
+
+module.exports = router;
