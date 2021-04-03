c3106e3cdd3e9f6c3503e2561f153c4f6f3be49f
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-03 07:14:20 +0530

    Signin route with jwt token

--- back-end/controllers/user/userController.js
+++ back-end/controllers/user/userController.js
@@ -1,5 +1,6 @@
 const userModel = require('../../models/userModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const signup = async (req, res) => {
     let { name, email, password } = req.body;
@@ -51,10 +52,10 @@ const signup = async (req, res) => {
 }
 
 const signin = async (req, res) => {
-    let { name, email, password } = req.body;
+    let { email, password } = req.body;
 
     try {
-        if (!name || !email || !password) {
+        if (!email || !password) {
             return res.status(422).json({
                 error: "please fill all fields"
             });
@@ -62,30 +63,22 @@ const signin = async (req, res) => {
 
         await userModel.findOne({ email: email })
             .then((savedUser) => {
-                if (savedUser) {
+                if (!savedUser) {
                     return res.status(404).json({
-                        error: "User already exists with this email"
+                        error: "invalid email or password"
+                    })
+                }
+                const isPasswordMatched = bcrypt.compareSync(password, savedUser.password)
+                if (isPasswordMatched) {
+                    const token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET)
+                    return res.status(200).json({
+                        message: "successfully signedin",
+                        token
                     })
                 } else {
-
-                    const hashedPassword = bcrypt.hashSync(password, 6)
-
-                    const newUser = new userModel({
-                        name,
-                        email,
-                        password: hashedPassword
-                    });
-
-                    newUser.save()
-                        .then(userData => {
-                            return res.status(200).json({
-                                message: "User registered successfully",
-                                data: userData
-                            })
-                        })
-                        .catch(err => {
-                            throw err;
-                        })
+                    return res.status(404).json({
+                        error: "invalid email or password"
+                    })
                 }
             })
             .catch(err => {
