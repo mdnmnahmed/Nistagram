5373300905bff4772dcbd3b4daee22b42586f849
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-04 20:24:30 +0530

    users signup functionality

--- client/src/components/screens/Signup.jsx
+++ client/src/components/screens/Signup.jsx
@@ -1,44 +1,69 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import M from 'materialize-css';
+import axios from 'axios';
 
 
 const Signup = () => {
     const history = useHistory();
+
     const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
-    const signUpData = () => {
-        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-            return M.toast({ html: "Invalid Email", classes: '#b71c1c red darken-4' })
+    const [password, setPassword] = useState("");
+
+    const signUpData = async (event) => {
+        event.preventDefault();
+
+        if (!name || !email || !password) {
+            return M.toast({ html: "Please fill all fields", classes: 'red darken-4' })
         }
-        fetch('/signup', {
-                method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
+        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            return M.toast({ html: "Invalid Email", classes: 'red darken-4' })
+        }
+
+        try {
+            const newUserData = {
                 name,
-                password,
-                email
-            })
-        }).then(res => res.json())
-        .then(data => {
-            if(data.error){
-                M.toast({ html: data.error, classes: '#b71c1c red darken-4' })
-            }else{
-                M.toast({ html: data.message, classes: '#33691e light-green darken-4' });
-                history.push('/signin');
+                email,
+                password
             }
-        }).catch(err => {
-            console.log("Error Occued : "+err);
-        })
+            const signedUpData = await axios.post(process.env.REACT_APP_API_URL + '/signup', newUserData);
+            M.toast({ html: signedUpData.data.message, classes: 'green darken-4' })
+            console.log('signedUpData res: ', signedUpData);
+            history.push('/signin');
+        } catch (err) {
+            console.log('error occured: ', err.response.data.error);
+            return M.toast({ html: err.response.data.error, classes: 'red darken-4' })
+        }
+
+        // fetch('/signup', {
+        //         method: "post",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify({
+        //         name,
+        //         password,
+        //         email
+        //     })
+        // }).then(res => res.json())
+        // .then(data => {
+        //     if(data.error){
+        //         M.toast({ html: data.error, classes: '#b71c1c red darken-4' })
+        //     }else{
+        //         M.toast({ html: data.message, classes: '#33691e light-green darken-4' });
+        //         history.push('/signin');
+        //     }
+        // }).catch(err => {
+        //     console.log("Error Occued : "+err);
+        // })
     }
 
     return (
-            <div className="myCard">
-                <div className="card auth-card">
-                    <h2 className="brand-logo">Nistagram</h2>
+        <div className="myCard">
+            <div className="card auth-card">
+                <h2 className="brand-logo">Nistagram</h2>
+                <form onSubmit={signUpData}>
                     <input type="text" placeholder="Name"
                         value={name}
                         onChange={(e) => setName(e.target.value)}
@@ -51,16 +76,17 @@ const Signup = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    <button onClick={signUpData} className="btn waves-effect waves-light">
+                    <button type="submit" className="btn waves-effect waves-light">
                         Signup
-                </button>
-                    <br />
-                    <br />
-                    <div>
-                        <Link to="/signin">Already have an <span style={{ color: '#26a69a' }}>Account</span></Link>
-                    </div>
+                    </button>
+                </form>
+                <br />
+                <br />
+                <div>
+                    <Link to="/signin">Already have an <span style={{ color: '#26a69a' }}>Account</span></Link>
                 </div>
             </div>
+        </div>
     )
 }
 
