1a466ae2b39eb71ed1f07b5c5d0f00132101660e
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-06 23:30:09 +0530

    Working on create posts

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -1,32 +1,54 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import M from 'materialize-css';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const CreatePost = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [image, setImage] = useState('');
     const [url, setUrl] = useState('');
+
+    const [image, setImage] = useState('');
+
     const history = useHistory();
 
-    const postDetails = () => {
-        const data = new FormData()
-            data.append("file", image)
-            data.append("upload_preset", "nistagram")
-            data.append("cloud_name", "nnn-nah")
+    const createPost = async (event) => {
+        event.preventDefault();
 
-        fetch("https://api.cloudinary.com/v1_1/nahnnn/image/upload", {
-            method: "post",
-            body: data
-        })
-            .then(res => res.json())
-            .then(data => {
-                setUrl(data.url)
-            })
-            .catch(err => {
-                console.log("Error occured while uploading pic : "+err);
-            });
-        
+        const fileData = await new FormData()
+        fileData.append("file", image)
+        fileData.append("upload_preset", "nistagram")
+        fileData.append("cloud_name", "nahnnn")
+
+        try {
+            const fileUploadData = await axios.post(process.env.REACT_APP_CLOUDINARY_API_BASE_URL + '/image/upload', fileData)
+                .catch(err => {
+                    throw new Error('Filed to Upload Picture, try again')
+                })
+            console.log('fileUploadData: ', fileUploadData);
+            setUrl(fileUploadData.data.url);
+
+            const newPostData = {
+                title,
+                body,
+                pic: fileUploadData.data.url
+            }
+
+            const token = {
+                
+            }
+
+            const uploadedPost = await axios.post(`${process.env.REACT_APP_API_URL}/post/create_post`, newPostData)
+            console.log(uploadedPost);
+            M.toast({ html: "Post uploaded", classes: 'light-green darken-4' });
+            history.push('/');
+
+        } catch (err) {
+            console.log('Error Occured : ', err);
+            M.toast({ html: err, classes: 'red darken-4' })
+        }
+
+        return;
         fetch('/createpost', {
             method: "post",
             headers: {
@@ -41,9 +63,9 @@ const CreatePost = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                if(data.error){
-                    M.toast({html: data.error, classes: '#b71c1c red darken-4'})
-                }else{
+                if (data.error) {
+                    M.toast({ html: data.error, classes: '#b71c1c red darken-4' })
+                } else {
                     M.toast({ html: "Post uploaded", classes: '#33691e light-green darken-4' });
                     history.push('/');
                 }
@@ -60,28 +82,33 @@ const CreatePost = () => {
             padding: '20px',
             textAlign: "center"
         }}>
-            <input type="text" placeholder="Title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            <input type="text" placeholder="Description"
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-            />
-            <div className="file-field input-field">
-                <div className="btn">
-                    <span>Upload Pic</span>
-                    <input type="file"
-                        onChange={(e) => setImage(e.target.files[0])}
-                    />
+            <form onSubmit={createPost}>
+                <input type="text" placeholder="Title" autoFill="on"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <input type="text" placeholder="Description"
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+
+                <div className="file-field input-field">
+                    <div className="btn">
+                        <span>Upload Pic</span>
+                        <input type="file"
+                            onChange={(e) => setImage(e.target.files[0])}
+                        />
+                    </div>
+                    <div className="file-path-wrapper">
+                        <input className="file-path validate" type="text" placeholder="Upload jpg, jpeg, gif, png only" />
+                    </div>
                 </div>
-                <div className="file-path-wrapper">
-                    <input className="file-path validate" type="text" placeholder="Upload jpg, jpeg, gif, png only" />
-                </div>
-            </div>
-            <button onClick={postDetails} type="file" className="btn waves-effect waves-dark #4dd0e1 cyan lighten-1">
-                Save Post
-            </button>
+
+                <button type="submit" className="btn waves-effect waves-dark #4dd0e1 cyan lighten-1">
+                    Save Post
+                </button>
+            </form>
         </div>
     )
 }
