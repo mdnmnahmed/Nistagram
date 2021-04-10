21ca9c007ec87914f51ecb175e38e15808f41fc5
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-10 08:00:24 +0530

    Added Protected routes functionality

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../App';
 
 const NavBar = () => {
+    const { state, dispatch } = useContext(UserContext);
+
+    const renderList = () => {
+        if (state) {
+            return [
+                <li><Link to="/createpost">Create Post</Link></li>,
+                <li><Link to="/profile">Profile</Link></li>
+            ]
+        } else {
+            return [
+                <li><Link to="/signin">Signin</Link></li>,
+                <li><Link to="/signup">Signup</Link></li>
+            ]
+        }
+    }
+
     return (
         <>
             <nav>
-                <div className="nav-wrapper white" style={{color: 'black'}}>
-                    <Link to="/" className="brand-logo left">Nistagram</Link>
+                <div className="nav-wrapper white" style={{ color: 'black' }}>
+                    <Link to={state ? '/' : '/signin'} className="brand-logo left">Nistagram</Link>
                     <ul id="nav-mobile" className="right">
-                        <li><Link to="/createpost">Create Post</Link></li>
-                        <li><Link to="/profile">Profile</Link></li>
-                        <li><Link to="/signin">Login</Link></li>
-                        <li><Link to="/signup">Signup</Link></li>
+                        {renderList()}
                     </ul>
                 </div>
             </nav>
