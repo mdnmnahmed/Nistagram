7ab7a48d78c2119b4d84f5ab4a2ffdc67f2e0af0
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-10 07:01:19 +0530

    Context API Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect, createContext, useReducer, useContext } from 'react';
 import NavBar from './components/NavBar';
 
 import './App.css';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
 
 //Screen Components
 import Home from './components/screens/Home';
@@ -12,29 +12,68 @@ import Signup from './components/screens/Signup';
 import Profile from './components/screens/Profile';
 import CreatePost from './components/screens/CreatePost';
 
-function App() {
+//Reducer & context
+import { reducer, initialState } from './reducers/userReducer';
+
+
+//Utils Functions
+import { getCookie } from './utils/CookiesHelper';
+
+//Context
+export const UserContext = createContext();
+
+const RoutingStuff = () => {
+    const history = useHistory();
+    const { state, dispatch } = useContext(UserContext);
+
+    useEffect(() => {
+        const userCookieData = getCookie('user_data');
+        if (userCookieData) {
+            userCookieData = JSON.parse(userCookieData);
+
+            dispatch({ type: 'USER', payload: userCookieData })
+
+            history.push('/');
+
+        } else {
+            history.push('/signin');
+        }
+    }, []);
+
     return (
         <>
-            <Router>
-                <NavBar />
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/signin">
-                        <Signin />
-                    </Route>
-                    <Route path="/signup">
-                        <Signup />
-                    </Route>
-                    <Route path="/profile">
-                        <Profile />
-                    </Route>
-                    <Route path="/createpost">
-                        <CreatePost />
-                    </Route>
-                </Switch>
-            </Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/signin">
+                    <Signin />
+                </Route>
+                <Route path="/signup">
+                    <Signup />
+                </Route>
+                <Route path="/profile">
+                    <Profile />
+                </Route>
+                <Route path="/createpost">
+                    <CreatePost />
+                </Route>
+            </Switch>
+        </>
+    )
+}
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <>
+            <UserContext.Provider value={{ state, dispatch }}>
+                <BrowserRouter>
+                    <NavBar />
+                    <RoutingStuff />
+                </BrowserRouter>
+            </UserContext.Provider>
         </>
     );
 }
