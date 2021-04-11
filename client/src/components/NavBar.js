58e57ad13162405884c1a0047c48b82600f70e09
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-11 13:27:34 +0530

    Warnings fixed in protected Routes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,22 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { UserContext } from '../App';
 
 const NavBar = () => {
+    const history = useHistory();
+
     const { state, dispatch } = useContext(UserContext);
 
     const renderList = () => {
         if (state) {
             return [
-                <li><Link to="/createpost">Create Post</Link></li>,
-                <li><Link to="/profile">Profile</Link></li>
+                <li key={Date.now()}><Link to="/createpost">Create Post</Link></li>,
+                <li key={Date.now()}><Link to="/profile">Profile</Link></li>
             ]
         } else {
             return [
-                <li><Link to="/signin">Signin</Link></li>,
-                <li><Link to="/signup">Signup</Link></li>
+                <li key={Date.now()}><Link to="/signin">Signin</Link></li>,
+                <li key={Date.now()}><Link to="/signup">Signup</Link></li>
             ]
         }
     }
