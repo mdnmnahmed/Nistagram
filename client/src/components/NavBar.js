07bf35d8210cf30040a0d3153cd048e85897c9e6
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-11 13:52:15 +0530

    user Logout Functionality with Context-API

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,17 +1,35 @@
 import React, { useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { UserContext } from '../App';
+import { setCookie } from '../utils/CookiesHelper';
 
 const NavBar = () => {
     const history = useHistory();
-
     const { state, dispatch } = useContext(UserContext);
+
+    /**
+     * users SignOut Handler
+     */
+    const handleSignOut = () => {
+        setCookie('user_token', null, 365)
+        setCookie('user_data', null, 365)
+
+        dispatch({ type: "CLEAR" })
+        history.push('/signin')
+    }
 
     const renderList = () => {
         if (state) {
             return [
                 <li key={Date.now()}><Link to="/createpost">Create Post</Link></li>,
-                <li key={Date.now()}><Link to="/profile">Profile</Link></li>
+                <li key={Date.now()}><Link to="/profile">Profile</Link></li>,
+                <li key={Date.now()}>
+                    <button className="btn waves-effect red"
+                        onClick={handleSignOut}
+                    >
+                        Logout
+                    </button>
+                </li>
             ]
         } else {
             return [
